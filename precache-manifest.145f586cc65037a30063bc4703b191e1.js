d3512393c15bf1d8cb00accde7c8ef0a2c6050f8
Rian Tavares <rian.tavares@live.com>
2019-09-28 14:32:56 -0300

    sending build to root

---
+++ precache-manifest.145f586cc65037a30063bc4703b191e1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "359338203497b98b8f6e9d5900d325b9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ae20781bdad72f7499ea",
+    "url": "/static/css/main.7beb4d76.chunk.css"
+  },
+  {
+    "revision": "ec10d94baeb8125278b8",
+    "url": "/static/js/2.4bed25e4.chunk.js"
+  },
+  {
+    "revision": "ae20781bdad72f7499ea",
+    "url": "/static/js/main.03d2bad8.chunk.js"
+  },
+  {
+    "revision": "961a2e02948b8b135940",
+    "url": "/static/js/runtime-main.d1a98e32.js"
+  }
+]);
