65a99442ef18f1740998aa90fcef81a6c22d225d
Rian Tavares <rian.tavares@live.com>
2019-09-29 00:18:32 -0300

    adding banner top responsive

--- aboutme/src/App.js
+++ aboutme/src/App.js
@@ -1,22 +1,11 @@
 import React from 'react';
-import './App.css';
+import BannerTop from './components/Banner'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Minha página está em manutenção :)</h1>
-        <p>
-          Me acompanha lá no insta enquanto isso <a href="https://www.instagram.com/riantba/" rel="noopener noreferrer">@riantba</a>.
-        </p>
-        <a
-          className="App-link"
-          href="https://github.com/RianTavares"
-          rel="noopener noreferrer"
-        >
-          Voltar para o Github
-        </a>
-      </header>
+      <BannerTop />
+      
     </div>
   );
 }
---
+++ aboutme/src/components/Banner/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Banner = () => {
+    return(
+        <>  
+            <header>
+                <div className="banner">
+
+                </div>
+            </header>
+
+        </>
+    );
+}
+
+export default Banner;
