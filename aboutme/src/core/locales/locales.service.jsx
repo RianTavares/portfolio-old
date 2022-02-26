81245501d7abc74a974a1baa426a37fae89518ce
Rian Tavares <rian.tavares@live.com>
2022-02-26 10:02:42 -0300

    integrate recommendations, set and style skeleton

--- aboutme/src/core/locales/locales.service.jsx
+++ aboutme/src/core/locales/locales.service.jsx
@@ -3,6 +3,12 @@ import i18n from 'i18n-js';
 const translate = (scope) =>
   i18n.t(scope);
 
+const getLocale = () => {
+  const localStorageVar = localStorage.getItem('locale@riantavares');
+  return localStorageVar || 'en';
+}
+
 export default {
   translate,
+  getLocale,
 };
--- aboutme/src/index.js
+++ aboutme/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import './assets/css/App.scss';
 import App from './App';
+import 'react-loading-skeleton/dist/skeleton.css'
 
 render((
     <App/>    
